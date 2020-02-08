d3a98ceacb7ea04f0d3521a71d1e3ae3aa0b0ae3
Nooralam khan <CoreyMShafer@gmail.com>
2020-02-08 12:45:10 +0600

    digital clock added

---
+++ main.js
@@ -0,0 +1,50 @@
+function showTime(){
+  let date =new Date();
+  let hours = date.getHours();//0-23
+  let minutes = date.getMinutes();//0-59
+  let seconds = date.getSeconds();//0-59
+
+  let formatHours = convertFormat(hours)
+
+  hours  = checkTime(hours);
+  hours  = addZero(hours);
+  minutes = addZero(minutes);
+  seconds = addZero(seconds);
+
+
+  document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds} ${formatHours}`;
+
+}
+
+
+
+function convertFormat(time){
+  let format = 'AM';
+
+  if(time >=12){
+    format = "PM";
+  }
+  return format;
+  }
+
+//12 hours system
+function checkTime(time){
+    if(time >12 ){
+      time = time -12;
+    }
+    if(time === 0 ){
+       time = 12
+    }
+    return time;
+  }
+
+  // if hour is single number then
+
+  function addZero(time){
+     if(time <10){
+        time ="0"+time;
+     }
+     return time;
+  }
+showTime()
+setInterval(showTime,1000);
